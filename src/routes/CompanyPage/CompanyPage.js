a0bae0c06a27bae1ccf00b8d08cccd4ce231108a
huyenng1220 <huyenng1220@gmail.com>
2021-04-30 17:48:30 -0700

    graph

--- src/routes/CompanyPage/CompanyPage.js
+++ src/routes/CompanyPage/CompanyPage.js
@@ -4,6 +4,7 @@ import NewsFeed from '../../components/NewsFeed/NewsFeed'
 import ApiService from '../../services/api-service';
 import CustomContext from '../../contexts/CustomContext';
 import './CompanyPage.css'
+import Plot from 'react-plotly.js';
 
 export default class CompanyPage extends Component {
   static contextType = CustomContext
@@ -13,20 +14,32 @@ export default class CompanyPage extends Component {
     },
   }
   state = {
-    time_data: [],
+    timeData: [],
+    priceData: [],
+
     company_data: {}
   };
 
   componentDidMount() {
     const { symbol } = this.props.match.params;
     var company = this.context.getCompany(symbol);
+    let timeDataValues = [];
+    let priceDataValues = [];
+    const pointerToThis = this;
     this.setState({company_data: company});
     this.context.clearError();
     ApiService.getTimingData(symbol)
         .catch(this.context.setError)
         .then((data) => {
-            this.setState({time_data: data});
-        });
+          for (var key in data['Time Series (Daily)']) {
+            timeDataValues.push(key);
+            priceDataValues.push(data['Time Series (Daily)'][key]['1. open']);
+          }
+          pointerToThis.setState({
+            timeData: timeDataValues,
+            priceData: priceDataValues
+          });
+                });
     if (!company) {
       console.log("fetched: " + symbol);
       return ApiService.getCompanyData(symbol)
@@ -44,13 +57,24 @@ export default class CompanyPage extends Component {
           <div>
             <h1>{c["Name"]} ({c["Symbol"]})</h1>
             <p>{c["Description"]}</p>
+            <Plot
+
+data={[
+  {
+    x: this.state.timeData,
+    y: this.state.priceData,
+    type: 'line',
+    mode: 'lines',
+    marker: {color: 'red'},
+  }
+]}
+layout={{width: 1000, height: 440, title: c["Symbol"]}}
+/>
           </div> : 
           <p className='red'>There was an error, try again</p>
         }
       
-        <Graph
-            time={this.state.time_data}
-        />
+      
         <NewsFeed
             newslist={[]}
         />
