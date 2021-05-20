8b10e477f52b8e578d949a27ed0ba8773e693bbd
Mason Qian <Thunderpurtz@Masons-MacBook-Pro-2.local>
2021-05-19 19:58:08 -0700

    localization and SEO

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import { Section } from "../../components/Utils/Utils";
 import "./LoginPage.css";
+import {Helmet} from "react-helmet";
 
 export default class LoginPage extends Component {
   static defaultProps = {
@@ -20,6 +21,10 @@ export default class LoginPage extends Component {
   render() {
     return (
       <main className="login-page-main">
+        <Helmet>
+                <title>Stock Clock - Login</title>
+                <meta name="description" content="Login and start managing your stocks on Stock Clock." />
+        </Helmet>
         <Section className="LoginPage">
           <h2>Login</h2>
           <LoginForm onLoginSuccess={this.handleLoginSuccess} />
